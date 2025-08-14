7899e53f5fb3f110fc51744521b318872a559fce
Darshan <darshan@Darshans-MacBook-Air.local>
2025-08-14 13:36:57 +0530

    Now Dashboard is Loading

--- frontend/src/components/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Card, Row, Col, ListGroup } from "react-bootstrap";
 import { useGetAdminDashboardDetailsQuery } from "../slices/dashboardApiSlice";
+import Loader from "./Loader";
 
 const AdminDashboard = () => {
   const [totalBooks, setTotalBooks] = useState(0);
@@ -30,121 +31,125 @@ const AdminDashboard = () => {
   return (
     <>
       <div style={{ height: 40 }}></div>
-      <Row>
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Total Books</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{totalBooks}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Row>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Total Books</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{totalBooks}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Total Users</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{totalUsers}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Total Users</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{totalUsers}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Total Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{totalOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Total Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{totalOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Amazon Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{amazonOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Amazon Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{amazonOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Flipkart Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{flipkartOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Flipkart Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{flipkartOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Kindle Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{kindleOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-      </Row>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Kindle Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{kindleOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
--- frontend/src/components/AuthorDashboard.jsx
+++ frontend/src/components/AuthorDashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Card, Row, Col, ListGroup } from "react-bootstrap";
 import { useGetAuthorDashboardDetailsQuery } from "../slices/dashboardApiSlice";
+import Loader from "./Loader";
 
 const AuthorDashboard = () => {
   const [totalBooks, setTotalBooks] = useState(0);
@@ -30,119 +31,123 @@ const AuthorDashboard = () => {
   return (
     <>
       <div style={{ height: 40 }}></div>
-      <Row>
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Total Books</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{totalBooks}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Total Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{totalOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong> Royalty</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>₹{totalRoyalty}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Row>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Total Books</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{totalBooks}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Total Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{totalOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong> Royalty</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>₹{totalRoyalty}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Amazon Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{amazonOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Amazon Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{amazonOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Flipkart Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{flipkartOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Flipkart Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{flipkartOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
 
-        <Col sm={12} md={6} lg={4} xl={4} className="my-3">
-          <Card>
-            <Row>
-              <Col>
-                <ListGroup>
-                  <ListGroup.Item>
-                    <h2>
-                      <strong>Kindle Orders</strong>
-                    </h2>
-                  </ListGroup.Item>
-                  <ListGroup.Item>
-                    <h2>{kindleOrders}</h2>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Col>
-            </Row>
-          </Card>
-        </Col>
-      </Row>
+          <Col sm={12} md={6} lg={4} xl={4} className="my-3">
+            <Card>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <h2>
+                        <strong>Kindle Orders</strong>
+                      </h2>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <h2>{kindleOrders}</h2>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
