e5114373b5cc6ebde147a8eacbd6b536a1339ad4
Darshan <darshan@Darshans-MacBook-Air.local>
2025-08-20 14:23:49 +0530

    Order Bug Fixed

--- backend/controllers/dashboardController.js
+++ backend/controllers/dashboardController.js
@@ -33,32 +33,99 @@ const getAdminDashboard = asyncHandler(async (req, res) => {
 const getAuthorDashboard = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
-  const totalRoyaltyData = await Order.aggregate([
-    {
-      $match: { author: req.user._id }, // filter orders by the logged-in author
-    },
-    {
-      $group: {
-        _id: null,
-        totalPlatformRoyalty: {
-          $sum: { $multiply: ["$platformRoyalty", "$numberofOrders"] },
+  if (user) {
+    const totalRoyaltyData = await Order.aggregate([
+      {
+        $match: { author: req.user._id }, // filter orders by the logged-in author
+      },
+      {
+        $group: {
+          _id: null,
+          totalPlatformRoyalty: {
+            $sum: { $multiply: ["$platformRoyalty", "$numberofOrders"] },
+          },
         },
       },
-    },
-  ]);
-  //console.log(totalRoyaltyData);
+    ]);
 
-  if (user) {
+    const totalOrdersData = await Order.aggregate([
+      { $match: { author: req.user._id } }, // filter by author
+      {
+        $group: {
+          _id: null,
+          totalOrdersNum: { $sum: "$numberofOrders" }, // sum the field
+        },
+      },
+    ]);
+
+    // Amazon
+    const amazonOrdersData = await Order.aggregate([
+      {
+        $match: {
+          author: req.user._id,
+          orderPlatform: "Amazon", // filter by platform also
+        },
+      }, // filter by author & orderPlatform
+      {
+        $group: {
+          _id: null,
+          amazonOrdersNum: { $sum: "$numberofOrders" }, // sum the field
+        },
+      },
+    ]);
+
+    // Flipkart
+    const flipkartOrdersData = await Order.aggregate([
+      {
+        $match: {
+          author: req.user._id,
+          orderPlatform: "Flipkart", // filter by platform also
+        },
+      }, // filter by author & orderPlatform
+      {
+        $group: {
+          _id: null,
+          flipkartOrdersNum: { $sum: "$numberofOrders" }, // sum the field
+        },
+      },
+    ]);
+
+    // Kindle
+    const kindleOrdersData = await Order.aggregate([
+      {
+        $match: {
+          author: req.user._id,
+          orderPlatform: "Kindle", // filter by platform also
+        },
+      }, // filter by author & orderPlatform
+      {
+        $group: {
+          _id: null,
+          kindleOrdersNum: { $sum: "$numberofOrders" }, // sum the field
+        },
+      },
+    ]);
+
     res.json({
       totalBooks: await Book.countDocuments({ author: req.user._id }),
-      totalOrders: await Order.countDocuments({ author: req.user._id }),
+      totalOrders:
+        totalOrdersData.length > 0 ? totalOrdersData[0].totalOrdersNum : 0, //await Order.countDocuments({ author: req.user._id }),
 
       totalRoyalty:
         totalRoyaltyData.length > 0
           ? totalRoyaltyData[0].totalPlatformRoyalty
           : 0,
 
-      amazonOrders: await Order.countDocuments({
+      amazonOrders:
+        amazonOrdersData.length > 0 ? amazonOrdersData[0].amazonOrdersNum : 0,
+      flipkartOrders:
+        flipkartOrdersData.length > 0
+          ? flipkartOrdersData[0].flipkartOrdersNum
+          : 0,
+      kindleOrders:
+        kindleOrdersData.length > 0 ? kindleOrdersData[0].kindleOrdersNum : 0,
+
+      /* amazonOrders: await Order.countDocuments({
         author: req.user._id,
         orderPlatform: "Amazon",
       }),
@@ -69,7 +136,7 @@ const getAuthorDashboard = asyncHandler(async (req, res) => {
       kindleOrders: await Order.countDocuments({
         author: req.user._id,
         orderPlatform: "Kindle",
-      }),
+      }),*/
     });
   } else {
     res.status(404);
